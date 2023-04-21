aef4a1878b0be699f4ca26b4b0f96338f1cb41fe
Adam <ahamiltonsut@paloaltonetworks.com>
2023-04-21 10:45:52 -0700

    Kubernetes Audit Settings Added

--- modules/access_rules.py
+++ modules/access_rules.py
@@ -7,6 +7,62 @@ def migrate(dst_session, src_session_list, options, logger):
 
     generic_pull_put_migrate.g_migrate(dst_session, src_session_list, 'Access Rules - Admission', '/api/v1/policies/admission', 'name', 'rules', logger, skip='owner', skip_value='system', col_dep=False)
 
+    #==================================================================================================================================================================================================================
+
+    #Const
+    MODULE = 'Access Rules - Kubernetes Audit Settings'
+    PULL_ENDPOINT = '/api/v1/settings/kubernetes-audit'
+    PUSH_ENDPOINT = '/api/v1/settings/kubernetes-audit'
+    DATA_INDEX = 'specifications'
+
+    #Logic
+    start_time = time.time()
+    logger.info(f'Starting {MODULE}s migration')
+
+    for src_session in tqdm(src_session_list, desc='Processing Consoles/Projects', leave=False, initial=1):
+        #Compare entities------------------------------------------------------
+        logger.debug(f'Comparing {MODULE}s from \'{src_session.tenant}\'')
+
+        #Pull entities
+        dst_res = dst_session.request('GET', PULL_ENDPOINT)
+        dst_entities = []
+        if dst_res.json():
+            dst_entities = dst_res.json().get(DATA_INDEX,[])
+
+
+        res = src_session.request('GET', PULL_ENDPOINT)
+        src_entities = []
+        if res.json():
+            src_entities = res.json().get(DATA_INDEX,[])
+        
+        #Compare entities
+        entities_to_migrate = []
+        if src_entities:
+            for ent in src_entities:
+                if src_session.tenant + ' - ' + ent['name'] not in [dst_ent['name'] for dst_ent in dst_entities]:
+                    ent['name'] = src_session.tenant + ' - ' + ent['name']
+                    entities_to_migrate.append(ent)
+
+        #Migrate entities------------------------------------------------------
+        if entities_to_migrate:
+            logger.debug(f'Migrating {MODULE}s')
+        else:
+            logger.debug(f'No {MODULE}s to migrate')
+            continue
+        
+        dst_entities.extend(entities_to_migrate)
+
+        payload = dst_entities
+
+        #Add entity
+        logger.info(f'Adding {MODULE} from \'{src_session.tenant}\'')
+        dst_session.request('POST', PUSH_ENDPOINT, json=payload)
+
+    end_time = time.time()
+    time_completed = round(end_time - start_time,3)
+    logger.info(f'{MODULE}s migration finished - {time_completed} seconds')
+
+    #==================================================================================================================================================================================================================
 
     #TODO
     #FIXME maybe but have to translate IDs from the src tenant to the DST tenant and that is not easy to do reliably. Would have to dump to file then read from that file. Map src_session_name - custom rule name TO rule ID since the IDs change during migration.
