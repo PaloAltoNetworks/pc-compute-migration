7482d27d49dd9540b17fe0217cbd96dfd33c230a
Adam <ahamiltonsut@paloaltonetworks.com>
2023-04-24 13:47:38 -0700

    Kb Access Rules updated

--- modules/access_rules.py
+++ modules/access_rules.py
@@ -3,9 +3,9 @@ import time
 from tqdm import tqdm
 
 def migrate(dst_session, src_session_list, options, logger):
-    # generic_pull_put_migrate.g_migrate(dst_session, src_session_list, 'Access Rules - Docker', '/api/v1/policies/docker', 'name', 'rules', logger)
+    generic_pull_put_migrate.g_migrate(dst_session, src_session_list, 'Access Rules - Docker', '/api/v1/policies/docker', 'name', 'rules', logger)
 
-    # generic_pull_put_migrate.g_migrate(dst_session, src_session_list, 'Access Rules - Admission', '/api/v1/policies/admission', 'name', 'rules', logger, skip='owner', skip_value='system', col_dep=False)
+    generic_pull_put_migrate.g_migrate(dst_session, src_session_list, 'Access Rules - Admission', '/api/v1/policies/admission', 'name', 'rules', logger, skip='owner', skip_value='system', col_dep=False)
 
     #==================================================================================================================================================================================================================
 
@@ -16,51 +16,51 @@ def migrate(dst_session, src_session_list, options, logger):
     DATA_INDEX = 'specifications'
 
     #Logic
-    # start_time = time.time()
-    # logger.info(f'Starting {MODULE}s migration')
+    start_time = time.time()
+    logger.info(f'Starting {MODULE}s migration')
 
-    # for src_session in tqdm(src_session_list, desc='Processing Consoles/Projects', leave=False, initial=1):
-    #     #Compare entities------------------------------------------------------
-    #     logger.debug(f'Comparing {MODULE}s from \'{src_session.tenant}\'')
+    for src_session in tqdm(src_session_list, desc='Processing Consoles/Projects', leave=False, initial=1):
+        #Compare entities------------------------------------------------------
+        logger.debug(f'Comparing {MODULE}s from \'{src_session.tenant}\'')
 
-    #     #Pull entities
-    #     dst_res = dst_session.request('GET', PULL_ENDPOINT)
-    #     dst_entities = []
-    #     if dst_res.json():
-    #         dst_entities = dst_res.json().get(DATA_INDEX,[])
+        #Pull entities
+        dst_res = dst_session.request('GET', PULL_ENDPOINT)
+        dst_entities = []
+        if dst_res.json():
+            dst_entities = dst_res.json().get(DATA_INDEX,[])
 
 
-    #     res = src_session.request('GET', PULL_ENDPOINT)
-    #     src_entities = []
-    #     if res.json():
-    #         src_entities = res.json().get(DATA_INDEX,[])
+        res = src_session.request('GET', PULL_ENDPOINT)
+        src_entities = []
+        if res.json():
+            src_entities = res.json().get(DATA_INDEX,[])
         
-    #     #Compare entities
-    #     entities_to_migrate = []
-    #     if src_entities:
-    #         for ent in src_entities:
-    #             if src_session.tenant + ' - ' + ent['name'] not in [dst_ent['name'] for dst_ent in dst_entities]:
-    #                 ent['name'] = src_session.tenant + ' - ' + ent['name']
-    #                 entities_to_migrate.append(ent)
+        #Compare entities
+        entities_to_migrate = []
+        if src_entities:
+            for ent in src_entities:
+                if src_session.tenant + ' - ' + ent['name'] not in [dst_ent['name'] for dst_ent in dst_entities]:
+                    ent['name'] = src_session.tenant + ' - ' + ent['name']
+                    entities_to_migrate.append(ent)
 
-    #     #Migrate entities------------------------------------------------------
-    #     if entities_to_migrate:
-    #         logger.debug(f'Migrating {MODULE}s')
-    #     else:
-    #         logger.debug(f'No {MODULE}s to migrate')
-    #         continue
+        #Migrate entities------------------------------------------------------
+        if entities_to_migrate:
+            logger.debug(f'Migrating {MODULE}s')
+        else:
+            logger.debug(f'No {MODULE}s to migrate')
+            continue
         
-    #     dst_entities.extend(entities_to_migrate)
+        dst_entities.extend(entities_to_migrate)
 
-    #     payload = dst_entities
+        payload = dst_entities
 
-    #     #Add entity
-    #     logger.info(f'Adding {MODULE} from \'{src_session.tenant}\'')
-    #     dst_session.request('POST', PUSH_ENDPOINT, json=payload)
+        #Add entity
+        logger.info(f'Adding {MODULE} from \'{src_session.tenant}\'')
+        dst_session.request('POST', PUSH_ENDPOINT, json=payload)
 
-    # end_time = time.time()
-    # time_completed = round(end_time - start_time,3)
-    # logger.info(f'{MODULE}s migration finished - {time_completed} seconds')
+    end_time = time.time()
+    time_completed = round(end_time - start_time,3)
+    logger.info(f'{MODULE}s migration finished - {time_completed} seconds')
 
     #==================================================================================================================================================================================================================
 
@@ -127,6 +127,16 @@ def migrate(dst_session, src_session_list, options, logger):
             #Add entity
             logger.info(f'Adding {MODULE} from \'{src_session.tenant}\'')
             dst_session.request('PUT', PUSH_ENDPOINT + "/" + str(payload['_id']), json=payload)
+
+        #Update ID list of kuberenttes rules
+        id_list = []
+        for ent_1 in entities_to_migrate:
+            if ent['type'] == "kubernetes-audit":
+                id_list.append(int(ent_1['_id']))
+
+        new_payload = {"_id":"kubernetesAudit","enabled":True,"customRulesIDs":id_list}
+        logger.info(f'Updating {MODULE} from \'{src_session.tenant}\'')
+        dst_session.request('PUT', "/api/v1/policies/kubernetes-audit", json=new_payload)
 
     end_time = time.time()
     time_completed = round(end_time - start_time,3)
